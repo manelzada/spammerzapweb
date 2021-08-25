9b63425452fd1308a0da42ba633649d6e00d99e2
manelzada <emmanoelsveiga@gmail.com>
2021-08-25 01:01:27 -0300

    1.1

--- spam.js
+++ spam.js
@@ -1,17 +1,18 @@
 var mensagem = prompt("Digite a mensagem");
 var cont = prompt("Quantas vezes?");
-var divTexto = document.getElementsByClassName("_13NKt copyable-text selectable-text")[1];
-var event = new InputEvent("input", {bubbles: true});
+var divTexto = document.getElementsByClassName("_13NKt copyable-text selectable-text")[1]; // Class da div onde digita as mensagens
+var ipEvento = new InputEvent("input", {bubbles: true});
 
 window.InputEvent = window.Event || window.InputEvent;
 
 
 for(let i = 0; i < cont; i++) {
   enviar(mensagem);
+  console.log("Funcionando"); // Teste
 }
 
 function enviar(mensagem) {
   divTexto.innerHTML = mensagem;
-  divTexto.dispatchEvent(event);
-  document.getElementsByClassName("_4sWnG")[0].click();
+  divTexto.dispatchEvent(ipEvento);
+  document.getElementsByClassName("_4sWnG")[0].click(); // Class do botao
 }
